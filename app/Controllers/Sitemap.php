2b08a9ee1aafcfe75524d6d099075cb4cd2a1744
hadiamr <amrullah29.id@gmail.com>
2024-08-24 08:42:29 +0700

    update

--- app/Controllers/Sitemap.php
+++ app/Controllers/Sitemap.php
@@ -3,11 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\NewsModel;
 
 class Sitemap extends BaseController
 {
     public function index()
     {
-        echo view('sitemap.xml');
+        $model = new NewsModel();
+        $data['url'] = $model->where('post_type', 'article')->orderBy('post_id', 'DESC')->asObject();
+        echo view('sitemap.xml', $data);
     }
 }
