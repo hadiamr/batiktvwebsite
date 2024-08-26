0d20da195ac6e3d7b7a85573b662b6a3db80004b
hadiamr <amrullah29.id@gmail.com>
2024-08-26 13:47:36 +0700

    up

---
+++ app/Views/sitemap.php
@@ -0,0 +1,21 @@
+<?php
+$datetime = new DateTime(date('Y-m-d H:i:s'));
+?>
+
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">>
+  <url>
+    <loc><?= base_url() ?></loc>
+    <lastmod><?= $datetime->format(DATE_ATOM); ?></lastmod>
+    <priority>1.0</priority>
+  </url>
+  <?php foreach ($url as $item) { ?>
+    <url>
+      <loc><?= 'https://' . base_url('/news/' . $item['post_title_seo']) ?></loc>
+      <lastmod><?= $datetime->format(DATE_ATOM); ?></lastmod>
+      <priority>0.8</priority>
+    </url>
+  <?php } ?>
+</urlset>
