cef3422bbab37969ce895a5bd9d1576433b82bae
hadiamr <amrullah29.id@gmail.com>
2024-09-25 12:36:22 +0700

    update error page

---
+++ app/Views/errors/error.php
@@ -0,0 +1,9 @@
+<link href="<?php echo base_url('home') ?>/css/styles.css" rel="stylesheet" />
+<div class="text-center">
+    <img class="mt-4" src="<?php echo base_url('home') ?>/assets/img/error.svg" alt="Error"></a>
+    <div>
+        <a class="start btn btn-primary mt-4" style="width: 120px" href="/">
+            HOME
+        </a>
+    </div>
+</div>
